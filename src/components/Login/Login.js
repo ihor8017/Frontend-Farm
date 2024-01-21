d189d70dfcf96de5a561b71b5d0f1019dbf9740a
Vitalii <kilativ147@gmail.com>
2024-01-21 22:37:39 +0200

    Change css for auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,42 +1,86 @@
-import React, { useContext, useState } from 'react';
-import { useAuth } from '../../hooks/AuthProvider';
-import { Link } from 'react-router-dom';
-import './login.css';
-import { Context } from '../..';
+import React, { useContext, useState } from 'react'
+import { useAuth } from '../../hooks/AuthProvider'
+import { NavLink } from 'react-router-dom'
+import { Context } from '../..'
 
 export function Login() {
-  const [input, setInput] = useState({
-    email: '',
-    password: '',
-  });
-  const {store} = useContext(Context);
-  //const auth = useAuth();
+	const [input, setInput] = useState({
+		email: '',
+		password: '',
+	})
+	const { store } = useContext(Context)
+	//const auth = useAuth();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (input.email !== '' && input.password !== '') {
-      store.login(input);
-    }
-  };
-  const handleInput = (e) => {
-    const { name, value } = e.target;
-    setInput((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if (input.email !== '' && input.password !== '') {
+			store.login(input)
+		}
+	}
+	const handleInput = (e) => {
+		const { name, value } = e.target
+		setInput((prev) => ({
+			...prev,
+			[name]: value,
+		}))
+	}
 
-  return (
-    <div className="auth-form-container">
-      <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">e-mail</label>
-        <input onChange={handleInput} type="email" placeholder="e-mail" id="email" name="email" />
-        <label htmlFor="password">password</label>
-        <input onChange={handleInput} type="password" placeholder="Пароль" id="password" name="password" />
-        <button type="submit">Увійти</button>
-      </form>
-      <Link className="link-btn" path='/signup'>Ви ще не зареєстровані?</Link>
-    </div>
-  );
+	return (
+		<div>
+			<h2 className='auth-title'>Login</h2>
+			<button type='button' className='auth-form-close'>
+				<img src='/img/svg/Cross.svg' alt='Close auth' />
+			</button>
+			<div className='auth-form-container'>
+				<form className='auth-form' onSubmit={handleSubmit}>
+					<div className="auth-logo">
+            <img src="/img/svg/Logo.svg" alt="" />
+          </div>
+          <h3 className='form-title'>Авторизація</h3>
+					<article className='form-article'>
+						<div className='form-field'>
+							<input
+								className='form-input-field'
+								onChange={handleInput}
+								type='email'
+								id='email'
+								name='email'
+							/>
+							<label className='form-label' htmlFor='email'>
+								e-mail
+							</label>
+						</div>
+					</article>
+					<article className='form-article'>
+						<div className='form-field'>
+							<input
+								className='form-input-field'
+								onChange={handleInput}
+								type='password'
+								id='password'
+								name='password'
+							/>
+							<label className='form-label' htmlFor='password'>
+								Пароль
+							</label>
+						</div>
+					</article>
+
+					<NavLink className='forgot-password' path='/forgot-password'>
+						Забули пароль?
+					</NavLink>
+
+					<button className='form-submit' type='submit'>
+						Увійти
+					</button>
+				</form>
+				<p className='auth-to-other-page'>
+					Ви ще не зареєстровані?
+					<span>
+						<NavLink path='/registration'> Sign up</NavLink>
+					</span>
+				</p>
+			</div>
+		</div>
+	)
 }
--- src/components/header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import s from './Header.module.css'
-
-const Header = () => {
-	return <div>Header</div>
-}
-
-export default Header
