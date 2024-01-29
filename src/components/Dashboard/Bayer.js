2cfad72ee70064cc7ae2b16cb9efa3c1f4cc8ff6
Ihor Chyzh <djar80@gmail.com>
2024-01-28 20:57:49 -0600

    adding logout component

--- src/components/Dashboard/Bayer.js
+++ src/components/Dashboard/Bayer.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logout from '../Logout/Logout';
 
 export default function Bayer ()  {
   return (
-    <h1>Bayer dashboard</h1>
+    <>
+      <h1>Bayer dashboard</h1>
+      <br></br>
+      <NavLink to="/main">Main</NavLink>
+      <br></br>
+      <Logout/>
+    </>
+
   );
 }
